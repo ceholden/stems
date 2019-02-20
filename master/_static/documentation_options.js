457ae53aa3ea236f84f90ca0d218d9506802118d
Chris Holden <ceholden@gmail.com>
2019-02-20 18:04:56 -0500

    Rebuild gh-pages docs on master: b756874

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.21.gae4c791.dirty',
+    VERSION: '0+untagged.22.gb756874.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
