75ebed5aad3857c6e70747e1a8a44ec849c43877
Chris Holden <ceholden@gmail.com>
2019-02-22 13:29:53 -0500

    Add RFC7946 fixer function and null geom checker

--- stems/gis/geom.py
+++ stems/gis/geom.py
@@ -1,6 +1,9 @@
 """ Geometry related functions
 """
+import json
+
 from rasterio.coords import BoundingBox
+import shapely.geometry
 
 from ..utils import list_like
 
@@ -55,3 +58,66 @@ def bbox_intersection(*bounds):
     top = min([b[3] for b in bounds])
 
     return BoundingBox(left, bottom, right, top)
+
+
+def is_null(geom):
+    """ Return True if a geometry is not valid / null
+
+    Parameters
+    ----------
+    shapely.geometry.BaseGeometry
+        Some shapely geometry (or something that supports
+        __geo_interface__)
+
+    Returns
+    -------
+    bool
+        Null or not
+    """
+    # asShape doesn't copy vs shape
+    geom_ = shapely.geometry.asShape(geom)
+    return getattr(geom_, '_geom', None) is None
+
+
+_CO_RFC7946 = ['RFC7946=YES', 'WRITE_BBOX=YES']
+def fix_geojson_rfc7946(geojson):
+    """ Fixes issues with dateline and meridian cutting
+
+    Parameters
+    ----------
+    geojson : dict
+        GeoJSON as a dict
+
+    Returns
+    -------
+    dict
+        Fixed up GeoJSON
+
+    TODO
+    ----
+    * Q1: Can we do this ourself using shapely/maths?
+    * Q2: Is solution to Q1 faster? Avoids dumps/loads
+    """
+    from osgeo import gdal, ogr
+    gdal.UseExceptions()
+    ogr.UseExceptions()
+
+    def read_vsi(filename):
+        vsifile = gdal.VSIFOpenL(filename,'r')
+        gdal.VSIFSeekL(vsifile, 0, 2)
+        vsileng = gdal.VSIFTellL(vsifile)
+        gdal.VSIFSeekL(vsifile, 0, 0)
+        return gdal.VSIFReadL(1, vsileng, vsifile)
+
+    # Fix up GeoJSON while writing to memory
+    _ = gdal.VectorTranslate('/vsimem/out.json',
+                             json.dumps(geojson),
+                             format='GeoJSON',
+                             layerCreationOptions=_CO_RFC7946)
+    _ = None  # make sure to delete so it flushes
+
+    text = (read_vsi('/vsimem/out.json')
+            .decode('utf-8')
+            .replace("'", '"'))
+
+    return json.loads(text)
