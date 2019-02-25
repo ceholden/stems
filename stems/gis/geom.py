dbe92a268139a62c3bf623f4e3eea4756d092842
Chris Holden <ceholden@gmail.com>
2019-02-25 16:44:34 -0500

    Import & test window utilities

--- stems/gis/geom.py
+++ stems/gis/geom.py
@@ -1,14 +1,23 @@
 """ Geometry related functions
 """
 import json
+import logging
+import math
 
+import affine
 from rasterio.coords import BoundingBox
+from rasterio.windows import Window, from_bounds
 import shapely.geometry
 
+from . import coords
 from ..utils import list_like
 
+logger = logging.getLogger(__name__)
 
-def bbox_union(*bounds):
+
+# =============================================================================
+# Bounds / BoundingBox
+def bounds_union(*bounds):
     """Return the union of some BoundingBox(s)
 
     Parameters
@@ -34,7 +43,7 @@ def bbox_union(*bounds):
     return BoundingBox(min(xs), min(ys), max(xs), max(ys))
 
 
-def bbox_intersection(*bounds):
+def bounds_intersection(*bounds):
     """Return the intersection of some BoundingBox(s)
 
     Parameters
@@ -60,6 +69,119 @@ def bbox_intersection(*bounds):
     return BoundingBox(left, bottom, right, top)
 
 
+def bounds_transform_union(bounds, transform):
+    """ Calculate union of bounds for some transform
+
+    Parameters
+    ----------
+    bounds : Sequence[BoundingBox]
+        Bounding boxes
+    transform : Affine
+        Affine transform to use
+
+    Returns
+    -------
+    rasterio.coords.BoundingBox
+        Union of BoundingBox
+    affine.Affine
+        Unified affine transform
+    tuple[int, int]
+        Shape of union
+    """
+    res = coords.transform_to_res(transform)
+    # Union of bounds
+    out_bounds = bounds_union(*bounds)
+    logger.debug('Initial output bounds: %r', out_bounds)
+
+    # Create transform
+    out_transform = affine.Affine.translation(out_bounds.left, out_bounds.top)
+    out_transform *= affine.Affine.scale(res[0], -res[1])
+    logger.debug('Output transform: %r', out_transform)
+
+    # Calculate width / height
+    out_w = int(math.ceil((out_bounds.right - out_bounds.left) / res[0]))
+    out_h = int(math.ceil((out_bounds.top - out_bounds.bottom) / res[1]))
+    logger.debug('Output width / height: %d / %d', out_w, out_h)
+
+    # Adjust bounds to fit (given integer width / height)
+    right, bottom = out_transform * (out_w, out_h)
+    out_bounds = BoundingBox(out_bounds.left, bottom, right, out_bounds.top)
+    logger.debug('Adjusted bounds: %r', out_bounds)
+
+    return out_bounds, out_transform
+
+
+def calculate_src_window(src_bounds, src_transform, dst_bounds):
+    """ Calculate the source window for some output bounds
+
+    Parameters
+    ----------
+    src_bounds : rasterio.coords.BoundingBox
+        Input bounds for the source raster image
+    src_transform : affine.Affine
+        Affine transform for the source raster image
+    dst_bounds : rasterio.coords.BoundingBox
+        Destination bounds
+
+    Returns
+    -------
+    rasterio.windows.Window
+        Window of source that contains ``dst_bounds``
+    rasterio.coords.BoundingBox
+        The BoundingBox representing the destination window for the source
+    """
+    # TODO: refactor to use bbox_union/intersect?
+    # Calculate source window and bounds (x/y offsets and sizes)
+    src_w, src_s, src_e, src_n = src_bounds
+    dst_w, dst_s, dst_e, dst_n = dst_bounds
+
+    left = src_w if src_w > dst_w else dst_w
+    bottom = src_s if src_s > dst_s else dst_s
+    right = src_e if src_e < dst_e else dst_e
+    top = src_n if src_n < dst_n else dst_n
+    adj_bounds = BoundingBox(left, bottom, right, top)
+
+    src_window = from_bounds(left, bottom, right, top,
+                             src_transform)
+    logger.debug('Computed source window: %r', src_window)
+
+    # Round the sizes
+    src_window = _round_window(src_window)
+    logger.debug('Rounded to source window: %r', src_window)
+
+    return src_window, adj_bounds
+
+
+def calculate_dst_window(src_bounds, dst_transform):
+    """ Calculate the rounded source window for some other transform
+
+    Parameters
+    ----------
+    src_bounds : rasterio.coords.BoundingBox
+        Input bounds for the source raster image
+    dst_transform : affine.Affine
+        Destination Affine transform
+
+    Returns
+    -------
+    rasterio.windows.Window
+        Window of source that conforms (pixel size, upper left) to
+        ``dst_tranform``
+    """
+    dst_window = from_bounds(*src_bounds, dst_transform)
+    logger.debug('Computed destination window: %r', dst_window)
+    dst_window = _round_window(dst_window)
+    logger.debug('Rounded to destination window: %r', dst_window)
+
+    return dst_window
+
+
+def _round_window(window):
+    return window.round_shape().round_offsets()
+
+
+# =============================================================================
+# Geometry
 def is_null(geom):
     """ Return True if a geometry is not valid / null
 
@@ -79,6 +201,8 @@ def is_null(geom):
     return getattr(geom_, '_geom', None) is None
 
 
+# =============================================================================
+# GEOJSON
 _CO_RFC7946 = ['RFC7946=YES', 'WRITE_BBOX=YES']
 def fix_geojson_rfc7946(geojson):
     """ Fixes issues with dateline and meridian cutting
--- stems/gis/tests/test_gis_geom.py
+++ stems/gis/tests/test_gis_geom.py
@@ -1,6 +1,8 @@
 """ Tests for :py:mod:`stems.gis.geom`
 """
+from affine import Affine
 from rasterio.coords import BoundingBox
+from rasterio.windows import Window
 import pytest
 
 from stems.gis import geom
@@ -9,28 +11,112 @@ BOX_1 = [0, 0, 10, 10]
 BOX_2 = [-5, -5, 5, 5]
 BOX_3 = [5, 5, 15, 15]
 
+AFFINE_1m = Affine(1., 0., 0., 0., -1., 10.)
+AFFINE_5m = Affine(5., 0., 0., 0., -5., 10.)
+
 
 # ----------------------------------------------------------------------------
-# bbox_union
+# bounds_union
 @pytest.mark.parametrize(('data', 'ans', ), [
     ((BOX_1, ), BoundingBox(0, 0, 10, 10)),
     ((BOX_1, BOX_2, ), BoundingBox(-5, -5, 10, 10)),
     ((BOX_1, BOX_2, BOX_3), BoundingBox(-5, -5, 15, 15)),
 ])
-def test_bbox_union(data, ans):
-    test = geom.bbox_union(*data)
+def test_bounds_union(data, ans):
+    test = geom.bounds_union(*data)
     assert isinstance(test, BoundingBox)
     assert test == ans
 
 
 # ----------------------------------------------------------------------------
-# bbox_intersection
+# bounds_intersection
 @pytest.mark.parametrize(('data', 'ans', ), (
     ((BOX_1, ), BoundingBox(0, 0, 10, 10)),
     ((BOX_1, BOX_2, ), BoundingBox(0, 0, 5, 5)),
     ((BOX_1, BOX_2, BOX_3), BoundingBox(5, 5, 5, 5)),
 ))
-def test_bbox_intersection(data, ans):
-    test = geom.bbox_intersection(*data)
+def test_bounds_intersection(data, ans):
+    test = geom.bounds_intersection(*data)
     assert isinstance(test, BoundingBox)
     assert test == ans
+
+
+# ----------------------------------------------------------------------------
+# union_bounds_transforms
+def test_bounds_tranform_union_1():
+    # Should all be the same
+    n = 3
+    bounds = BoundingBox(365385.0, 2901915.0, 380385.0, 2916915.0)
+    transform = Affine(30.0, 0.0, 365385.0, 0.0, -30.0, 2916915.0)
+
+    test = geom.bounds_transform_union((bounds, ) * n, transform)
+    assert test[0] == bounds
+    assert test[1] == transform
+
+
+@pytest.mark.parametrize(('bounds', 'transform', 'result'), [
+    [(BOX_1, BOX_2,), AFFINE_1m,
+     (BoundingBox(-5, -5, 10, 10), Affine(1., 0., -5, 0, -1., 10), (15, 15))],
+    [(BOX_1, BOX_2,), AFFINE_5m,
+     (BoundingBox(-5, -5, 10, 10), Affine(5., 0., -5, 0, -5., 10), (3, 3))],
+    [(BOX_1, BOX_2, BOX_3), AFFINE_1m,
+     (BoundingBox(-5, -5, 15, 15), Affine(1., 0., -5, 0, -1., 15), (20, 20))]
+])
+def test_bounds_tranform_union_2(bounds, transform, result):
+    ans = geom.bounds_transform_union(bounds, transform)
+    assert len(ans) == 2
+    assert isinstance(ans[0], BoundingBox)
+    assert isinstance(ans[1], Affine)
+    assert ans[0] == result[0]
+    assert ans[1] == result[1]
+
+
+# ----------------------------------------------------------------------------
+# calculate_src_window
+@pytest.mark.parametrize(('bounds', 'transform', 'dst_bounds', 'result'), [
+    (BOX_1, AFFINE_1m, BOX_2, (Window(0, 5, 5, 5), BoundingBox(0, 0, 5, 5))),
+    (BOX_1, AFFINE_5m, BOX_2, (Window(0, 1, 1, 1), BoundingBox(0, 0, 5, 5))),
+    (BOX_1, AFFINE_5m, BOX_3, (Window(1, 0, 1, 1), BoundingBox(5, 5, 10, 10)))
+])
+def test_calculate_src_window_exact(bounds, transform, dst_bounds, result):
+    ans = geom.calculate_src_window(bounds, transform, dst_bounds)
+    assert ans[0] == result[0]
+    assert ans[1] == result[1]
+
+
+def test_calculate_src_window_nonexact():
+    bounds = BoundingBox(0, 0, 10, 10)
+    transform = Affine(5., 0., 0., 0., -5., 10.)
+    dst_bounds = BoundingBox(-1., 0., 12, 13)
+    ans = geom.calculate_src_window(bounds, transform, dst_bounds)
+    assert ans[0] == Window(0, 0, 2, 2)
+    assert ans[1] == bounds
+
+
+# ----------------------------------------------------------------------------
+# calculate_dst_window
+def test_calculate_dst_window():
+    src_bounds = BoundingBox(0, 0, 120, 120)
+    dst_transform = Affine(30., 0., 0,
+                           0., -30., 120)
+    ans = geom.calculate_dst_window(src_bounds, dst_transform)
+    assert ans == Window(0, 0, 4, 4)
+
+    # dst_transform further left & up
+    src_bounds = BoundingBox(0, 0, 65, 95)
+    dst_transform = Affine(30., 0., -35,
+                           0., -30., 155)
+    ans = geom.calculate_dst_window(src_bounds, dst_transform)
+    assert ans == Window(1, 2, 2, 3)
+
+
+# ----------------------------------------------------------------------------
+# is_null
+def test_is_null():
+    pass
+
+
+# ----------------------------------------------------------------------------
+# fix_geojson_rfc7946
+def test_fix_geojson_rfc7946():
+    pass
