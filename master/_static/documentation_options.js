26016ebe2889a5aa48658881674e9a43ae8a4463
TRAVIS-CI <ceholden@gmail.com>
2019-04-18 16:54:08 +0000

    Rebuild gh-pages docs on master: beab386

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1+12.g9962058',
+    VERSION: '0.0.1+15.gbeab386',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
