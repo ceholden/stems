1892305b584c20424678ea7627812582a2bafb05
TRAVIS-CI <ceholden@gmail.com>
2019-08-06 17:09:24 +0000

    Rebuild gh-pages docs on master: bf99a6e

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.3+4.g7e4f129',
+    VERSION: '0.0.3+6.gbf99a6e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
